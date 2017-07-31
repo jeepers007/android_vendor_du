70e4ef324939aa059eb52a8e3187db3ec3cc5d28
jeepers007 <jstn.haggard@gmail.com>
2017-07-31 11:15:23 -0500

    Revert "Remove OP2 from official support"
    This reverts commit 12afd33f78c7fc008c37aa88e959742f14c4339b.

--- caf-vendorsetup.sh
+++ caf-vendorsetup.sh
@@ -2,6 +2,7 @@ add_lunch_combo du_cheeseburger-userdebug
 add_lunch_combo du_clark-userdebug
 add_lunch_combo du_d802-userdebug
 add_lunch_combo du_lux-userdebug
+add_lunch_combo du_oneplus2-userdebug
 add_lunch_combo du_oneplus3-userdebug
 add_lunch_combo du_osprey-userdebug
 add_lunch_combo du_pme-userdebug
